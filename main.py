7a06bece7f9d82087280b6a523b19a5a65d4c7d5
climbingg <xuezhiqian759@gmail.com>
2024-07-01 09:41:57 +0800

    變更main.py

--- main.py
+++ main.py
@@ -35,3 +35,40 @@ pygame.mixer.music.load(os.path.join("sound", "background.ogg"))
 
 # 無限重複播放
 pygame.mixer.music.play(-1)
+
+class Player(pygame.sprite.Sprite):
+    """玩家"""
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.image = pygame.transform.scale(pygame.image.load("img/player.png"), (512, 512))
+        self.rect = self.image.get_rect()
+        self.rect.centerx = WIDTH / 2
+        self.rect.centery = HEIGHT - 250
+
+    def update(self) -> None:
+        """更新畫面"""
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT]:
+            self.rect.x -= 20
+        if keys[pygame.K_RIGHT]:
+            self.rect.x += 20
+        self.rect.clamp_ip(screen.get_rect())
+
+airwar = Player()
+
+all_sprites.add(airwar)
+
+clock = pygame.time.Clock()
+running = True
+t = time.time()
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    clock.tick(FPS)
+    screen.fill(BLACK)
+    all_sprites.update()
+    # screen.blit(board, (0, 0))
+    all_sprites.draw(screen)
